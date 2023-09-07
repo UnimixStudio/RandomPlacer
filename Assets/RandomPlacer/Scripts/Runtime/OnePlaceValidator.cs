f7c251d836bceb3ba6bfc3486bf6b9b38209d051
UnimixStudio <63611889+UnimixStudio@users.noreply.github.com>
2023-09-07 10:33:05 +0600

    v1.1.1

---
+++ Assets/RandomPlacer/Scripts/Runtime/OnePlaceValidator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace RandomPlacer
+{
+	[RequireComponent(typeof(Placer))]
+	public class OnePlaceValidator : MonoBehaviour
+	{
+		[SerializeField] private Placer _placer;
+
+		private void OnValidate()
+		{
+			_placer ??= GetComponent<Placer>();
+
+			if (transform.childCount == 0)
+				_placer.Place();
+		}
+	}
+}
